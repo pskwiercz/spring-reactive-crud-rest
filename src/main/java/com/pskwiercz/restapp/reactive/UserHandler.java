75f3ac1aa63b412f1c0730de38989075514b43f7
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2020-04-15 13:23:38 +0200

    Chapter 4 - Add reactive CRUD

--- src/main/java/com/pskwiercz/restapp/reactive/UserHandler.java
+++ src/main/java/com/pskwiercz/restapp/reactive/UserHandler.java
@@ -6,6 +6,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
 
 public class UserHandler {
     private final UserRepository userRepository;
@@ -17,5 +18,29 @@ public class UserHandler {
     public Mono<ServerResponse> getAllUsers(ServerRequest request) {
         Flux<User> users = this.userRepository.getAllUsers();
         return ServerResponse.ok().contentType(APPLICATION_JSON).body(users, User.class);
+    }
+
+    public Mono<ServerResponse> getUser(ServerRequest request){
+        int userId = Integer.valueOf(request.pathVariable("id"));
+        Mono<ServerResponse> notFound = ServerResponse.notFound().build();
+        Mono<User> userMono = this.userRepository.getUser(userId);
+        return userMono
+                .flatMap(user -> ServerResponse.ok().contentType(APPLICATION_JSON).body(fromObject(user)))
+                .switchIfEmpty(notFound);
+    }
+
+    public Mono<ServerResponse> createUser(ServerRequest request) {
+        Mono<User> user = request.bodyToMono(User.class);
+        return ServerResponse.ok().build(this.userRepository.saveUser(user));
+    }
+
+    public Mono<ServerResponse> updateUser(ServerRequest request) {
+        Mono<User> user = request.bodyToMono(User.class);
+        return ServerResponse.ok().build(this.userRepository.updateUser(user));
+    }
+
+    public Mono<ServerResponse> deleteUser(ServerRequest request) {
+        int userId = Integer.valueOf(request.pathVariable("id"));
+        return ServerResponse.ok().build(this.userRepository.deleteUser(userId));
     }
 }
